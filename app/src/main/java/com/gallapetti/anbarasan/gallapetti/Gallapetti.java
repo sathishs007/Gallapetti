fcfef02d98597778e04098ea874655986f402d2d
Anbarasan <anbganapathy@paypal.com>
2016-11-15 16:46:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/gallapetti/anbarasan/gallapetti/Gallapetti.java
@@ -0,0 +1,13 @@
+package com.gallapetti.anbarasan.gallapetti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Gallapetti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallapetti);
+    }
+}
